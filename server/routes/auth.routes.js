f6b028bc6fcfc2705d6b9443c28be7cbc0be5078
Usman Ismail <usmanismailcs55@gmail.com>
2026-09-16 12:29:44 +0500

    Add JWT authentication token generation

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,7 +1,7 @@
-
 const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const generateToken = require("../utils/generateToken");
 
 const router = express.Router();
 
@@ -45,7 +45,10 @@ router.post("/login", async (req, res) => {
       });
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      user.password
+    );
 
     if (!isPasswordValid) {
       return res.status(401).json({
@@ -53,9 +56,18 @@ router.post("/login", async (req, res) => {
       });
     }
 
+    // Generate JWT token
+    const token = generateToken(user._id);
+
     res.status(200).json({
       message: "Login successful",
-      email: user.email,
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        balance: user.balance,
+      },
     });
   } catch (error) {
     console.error(error);
@@ -67,4 +79,3 @@ router.post("/login", async (req, res) => {
 });
 
 module.exports = router;
-
